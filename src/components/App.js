157047daf001b1a1c312ae189a776a15e15fcd70
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-11-21 08:16:30 +0000

    changes whole code according to test case

--- src/components/App.js
+++ src/components/App.js
@@ -1,69 +1,59 @@
-import React, { useState, useEffect } from "react";
-import Tours from "./Tours";
+import React, { useEffect, useState } from "react";
+import Tours from "./Tours"
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [filteredTour, setFilteredTour] = useState(Tours);
-  const [showFullInfoFor, setShowFullInfoFor] = useState(null);
+  const [data, setData] = useState(null)
+  const [isVisibleStatuses, setVisibleStatuses] = useState(Array(Tours.length).fill(false))
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 2000)
+    setTimeout(() => setData(Tours), 2000)
   }, []);
 
 
+  function deleteData(i) {
+    const newData = data.filter((el, ind) => ind != i)
+    setData(newData)
+  }
+
+  function toggleVisibleStatus(i) {
+    const newVisibleStatus = [...isVisibleStatuses]
+    newVisibleStatus[i] = !newVisibleStatus[i]
+    setVisibleStatuses(newVisibleStatus)
+  }
+
   function handleRefresh() {
     window.location.reload();
-    setFilteredTour(Tours);
-  }
-
-  const handleDelete = (id) => {
-    setFilteredTour((preTour) => preTour.filter((Tours) => Tours.id !== id));
-  }
-
-  const handleToggleInfo = (id) => {
-    setShowFullInfoFor(id === showFullInfoFor ? null : id);
   }
 
   return (
     <main id="main">
+      <h1 className="title" >Tours</h1>
       {
-        loading ?
-          (<h1>Loading...</h1>) :
+        !data ?
+          <p className="loading" >Loading</p>
+          :
           (
-            filteredTour.length ?
+            data.length == 0 ?
+              <div>
+                <p>No more tours</p>
+                <button className="btn" onClick={handleRefresh} >Refresh</button>
+              </div>
+              :
               (
-                filteredTour.map((tour, index) => (
-                  <div key={index}>
-                    <img src={tour.image}></img>
-                    <h4 className="title">{tour.name}</h4>
-                    <p className="tour-price">{tour.price}</p>
-                    <div id={`tour-item-para-${tour.id}`}>
-                      {
-                        showFullInfoFor === tour.id ?
-                          <div>
-                            <p className="tour-info">{tour.info}</p>
-                            <button className="show-less-btn" onClick={() => handleToggleInfo(tour.id)}>
-                              See Less
-                            </button>
-                          </div>
-                          :
-                          <div>
-                            <p id={`see-more-${tour.id}`}>{`${tour.info.slice(0, 200)}`}</p>
-                            <button className="show-more-btn" onClick={() => handleToggleInfo(tour.id)}>
-                              See More
-                            </button>
-                          </div>
-                      }
+                data.map((element, i) => {
+                  return (
+                    <div className="single-tour" >
+                      <p className="tour-info" >{element.price}</p>
+                      <p id={`tour-item-para-${element.id}`} className="tour-price" >
+                        {
+                          isVisibleStatuses[i] ? element.info : element.info.slice(0, 200)
+                        }
+                      </p>
+                      <button id={`see-more-${element.id}`} onClick={() => toggleVisibleStatus(i)} >{!isVisibleStatuses[i] ? "See more" : "Show less"}</button>
+                      <button className="delete-btn" id={`delete-btn-${element.id}`} onClick={() => deleteData(i)} >Delete</button>
                     </div>
-                    <button id={`delete-btn-${tour.id}`} value={tour.id} onClick={() => handleDelete(tour.id)}>Delete Tour</button>
-                  </div>
-                ))
-              ) :
-              (
-                <div>
-                  <h3>No tours left</h3>
-                  <button className="btn" onClick={handleRefresh}>Refresh</button>
-                </div>
+                  )
+                })
               )
           )
       }
